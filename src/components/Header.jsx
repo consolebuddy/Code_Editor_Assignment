6b5192eb333281fbef42a0bc5cf0e927394f65ac
consolebuddy <36247021+consolebuddy@users.noreply.github.com>
2024-06-28 12:50:27 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <>
+            <h1>A Simple Code Editor (TealFeed Assignment)</h1>
+            <h2>
+                -by Palen Pushkar
+            </h2>
+        </> 
+
+    )
+}
+
+export default Header;
